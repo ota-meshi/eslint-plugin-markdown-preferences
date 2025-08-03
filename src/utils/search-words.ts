7c8678e3d01adf31554ed87ac23ad2508c378600
Yosuke Ota <otameshiyo23@gmail.com>
2025-08-03 17:56:32 +0900

    feat: add ignores option (#20)
    * feat: add ignores option
    
    * Create slimy-planes-warn.md
    
    * Create healthy-bottles-jog.md

--- src/utils/search-words.ts
+++ src/utils/search-words.ts
@@ -1,5 +1,6 @@
 import type { MarkdownSourceCode } from "@eslint/markdown";
 import type { SourceLocation } from "estree";
+import type { JSONSchema4 } from "json-schema";
 import type { Text } from "mdast";
 
 export const RE_BOUNDARY =
@@ -8,11 +9,17 @@ export const RE_BOUNDARY =
 /**
  * Iterate through words in a text node that match the specified words.
  */
-export function* iterateSearchWords(
-  sourceCode: MarkdownSourceCode,
-  node: Text,
-  words: string[],
-): Generator<{
+export function* iterateSearchWords({
+  sourceCode,
+  node,
+  words,
+  ignores,
+}: {
+  sourceCode: MarkdownSourceCode;
+  node: Text;
+  words: string[];
+  ignores: SearchWordsIgnoreContext;
+}): Generator<{
   word: string;
   loc: SourceLocation;
   range: [number, number];
@@ -20,6 +27,7 @@ export function* iterateSearchWords(
   const text = sourceCode.getText(node);
 
   for (const word of words) {
+    if (ignores.ignore(word)) continue;
     let startPosition = 0;
     while (true) {
       const index = text.indexOf(word, startPosition);
@@ -51,4 +59,108 @@ export function* iterateSearchWords(
       };
     }
   }
+}
+
+export type Ignore = {
+  words?: string[] | string;
+  node?: Record<string, unknown>;
+};
+export type Ignores = Ignore[];
+
+export const IGNORES_SCHEMA: JSONSchema4 = {
+  type: "array",
+  items: {
+    type: "object",
+    properties: {
+      words: {
+        anyOf: [
+          {
+            type: "string",
+          },
+          {
+            type: "array",
+            items: {
+              type: "string",
+            },
+          },
+        ],
+      },
+      node: {
+        type: "object",
+      },
+    },
+    additionalProperties: true,
+  },
+};
+
+export type SearchWordsIgnoreContext = {
+  ignore(word: string): boolean;
+  enter: (node: Record<string, unknown>) => void;
+  exit: (node: Record<string, unknown>) => void;
+};
+
+/**
+ * Create a context for ignoring specific words or nodes.
+ */
+export function createSearchWordsIgnoreContext(
+  ignores: Ignores | undefined,
+): SearchWordsIgnoreContext {
+  if (!ignores || ignores.length === 0) {
+    return {
+      enter: () => undefined,
+      exit: () => undefined,
+      ignore: () => false,
+    };
+  }
+
+  type IgnoreCondition = {
+    isIgnoreWord: (word: string) => boolean;
+    isIgnoreNode: (node: Record<string, unknown>) => boolean;
+  };
+  const conditions: IgnoreCondition[] = ignores.map((ignore) => {
+    const isIgnoreWord: (word: string) => boolean =
+      ignore.words == null
+        ? () => true
+        : Array.isArray(ignore.words)
+          ? (word) => (ignore.words as string[]).includes(word)
+          : (word) => ignore.words === word;
+    const node = ignore.node || {};
+    const keys = Object.keys(node);
+    return {
+      isIgnoreWord,
+      isIgnoreNode: (nodeToCheck) => {
+        return keys.every((key) => nodeToCheck[key] === node[key]);
+      },
+    };
+  });
+
+  const currentIgnores = new Set<{
+    node: Record<string, unknown>;
+    condition: IgnoreCondition;
+  }>();
+
+  return {
+    enter(node) {
+      for (const ignore of conditions) {
+        if (ignore.isIgnoreNode(node)) {
+          currentIgnores.add({ node, condition: ignore });
+        }
+      }
+    },
+    exit(node) {
+      for (const element of [...currentIgnores]) {
+        if (element.node === node) {
+          currentIgnores.delete(element);
+        }
+      }
+    },
+    ignore(word) {
+      for (const { condition } of currentIgnores) {
+        if (condition.isIgnoreWord(word)) {
+          return true;
+        }
+      }
+      return false;
+    },
+  };
 }
