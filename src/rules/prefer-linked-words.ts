0f0790a8ac7931decf652855b40060d33a8ad4e8
yosuke ota <otameshiyo23@gmail.com>
2025-08-03 09:09:04 +0900

    fix(prefer-linked-words): false positive with CJK characters

--- src/rules/prefer-linked-words.ts
+++ src/rules/prefer-linked-words.ts
@@ -6,7 +6,7 @@ type WordsObject = Record<string, string | null>;
 type Words = WordsObject | string[];
 
 const RE_BOUNDARY =
-  /^[\s\p{Letter_Number}\p{Modifier_Letter}\p{Modifier_Symbol}\p{Nonspacing_Mark}\p{Other_Letter}\p{Other_Symbol}\p{Script=Han}!"#$%&'(),./:;<=>?\\{|}~\u{3001}-\u{303d}\u{3220}-\u{3229}\u{3248}-\u{324f}\u{3251}-\u{325f}\u{3280}-\u{3289}\u{32b1}-\u{32bf}]*$/u;
+  /^[\s\p{Letter_Number}\p{Modifier_Letter}\p{Modifier_Symbol}\p{Nonspacing_Mark}\p{Other_Letter}\p{Other_Symbol}\p{Script=Han}!"#$%&'(),./:;<=>?\\{|}~\u{2ffc}-\u{303d}\u{30a0}-\u{30fb}\u{3192}-\u{32bf}\u{fe10}-\u{fe1f}\u{fe30}-\u{fe6f}\u{ff00}-\u{ffef}\u{2ebf0}-\u{2ee5d}]*$/u;
 
 export default createRule<[{ words?: Words }?]>("prefer-linked-words", {
   meta: {
